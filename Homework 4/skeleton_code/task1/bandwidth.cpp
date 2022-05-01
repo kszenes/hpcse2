fca16adf1b82bf40ccea8f0eb7ec3b4a63e7eb31
Kalman Szenes <kszenes@ethz.ch>
2022-05-01 23:43:21 +0200

    commented bandwidth.cpp

--- Homework 4/skeleton_code/task1/bandwidth.cpp
+++ Homework 4/skeleton_code/task1/bandwidth.cpp
@@ -21,7 +21,9 @@ int main(int argc, char *argv[])
 
     //TODO: Define correct min and max message sizes
     const size_t min_size = 1;        // this should be 1 byte
-    const size_t max_size = 16777216;        // this should be 16 MB
+    const size_t max_size = 1 << 24;        // this should be 16 MB
+
+    std::vector<MPI_Request> requests(2*iterations);
 
     std::ofstream fout;
     if (rank == 0)
@@ -35,25 +37,36 @@ int main(int argc, char *argv[])
     }
     for (size_t msg_size = min_size; msg_size <= max_size; msg_size *= 2)
     {
-	    double time = -MPI_Wtime();
 
         //TODO: perform send/receive of message with size msg_size and measure how long 
         //      it takes for communication to complete
-        std::vector<int> msg;
-        msg.reserve(msg_size);
+      std::vector<char> msg(msg_size);
+	    double time = -MPI_Wtime();
         if (rank == 0) {
-            MPI_Send(msg.data(), 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            // Get reliable estimate of bandwidth with averaging over iterations
+            for (size_t i = 0; i < iterations; i++) {
+              // Send non blocking message
+              MPI_Isend(msg.data(), msg_size, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &requests[i]);
+
+              // MPI_Send(msg.data(), 1, MPI_INT, 1, i, MPI_COMM_WORLD);
+            }
         } else {
-            MPI_Recv(msg.data(), 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            for (size_t i = 0; i < iterations; i++) {
+              // Non blocking message
+              MPI_Irecv(msg.data(), msg_size, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &requests[i]);
+              // MPI_Recv(msg.data(), 1, MPI_INT, 0, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
         }
 
+        // Wait for messages to be sent and received
+        MPI_Waitall(iterations, requests.data(), MPI_STATUS_IGNORE);
         MPI_Barrier(MPI_COMM_WORLD);
         time += MPI_Wtime();
 
         if (rank == 0) 
         {
             //TODO: Compute bandwidth
-            const double bandwidth = msg_size / time;
+            const double bandwidth = msg_size * iterations / time;
             fout << msg_size << '\t' << bandwidth << '\n';
         }
     }
